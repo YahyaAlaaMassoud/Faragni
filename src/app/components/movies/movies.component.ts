ace84b23addd893989ed70d1a97b33471c1d9a1c
Yahya Alaa <yehiaalaamassoud@gmail.com>
2017-10-28 00:01:16 +0200

    tabs, carousel, cards, some css

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit } from '@angular/core';
 //animations
 import { fadeInAnimation } from '../../animations/fade-in.animation'
 
+//models
+import { Movie } from '../../models/movie.model'
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -12,9 +15,15 @@ import { fadeInAnimation } from '../../animations/fade-in.animation'
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[];
+
+  constructor() {
+    this.movies = [];
+  }
 
   ngOnInit() {
+    this.movies = JSON.parse(localStorage.getItem('movies'));
+    console.log(this.movies)
   }
 
 }
