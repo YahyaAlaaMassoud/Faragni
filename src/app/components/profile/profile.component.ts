c93b2f229111b26d1c1c02f30583252aaac19020
unknown <khaledawaled@gmail.com>
2017-11-27 21:00:31 +0200

    upload image save local storage - Followers cards

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../models/user.model';
-import { Event } from 'angular-bootstrap-md/utils/facade/browser';
+import { Event, document } from 'angular-bootstrap-md/utils/facade/browser';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -9,19 +9,27 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
+  @ViewChild('fileInput') fileInput;
   currentUser: User;
   isEdit: boolean;
   myBio: String;
   showFollowers: boolean;
+  currentScreen:number;
+  base64;
+  yourmodel;
+  logo;
   constructor(private router:Router) {
       this.currentUser = JSON.parse(localStorage.getItem('currentUser'));     
-      this.currentUser.Email=["khaledawaled@live.com"];
+     // this.currentUser.Email=["khaledawaled@live.com"];
       this.currentUser.JoiningDate = new Date("May");
       this.currentUser.Age = 21;
+      this.currentUser.bio = "";
+      //localStorage.setItem('currentUser',JSON.stringify(this.currentUser));
+      this.currentScreen = 0;
       this.isEdit = false;
-      this.myBio = "";
       this.showFollowers=false;
+      console.log(this.currentUser);
+      
   }
 
   ngOnInit() {
@@ -29,16 +37,37 @@ export class ProfileComponent implements OnInit {
   }
   takeAction(element){
     this.isEdit = !this.isEdit;
-    if(this.isEdit)
+    var myimg = document.getElementById("pfimg");
+    if(this.isEdit){
        element.textContent = "SAVE";
-    else
+        myimg.className=myimg.className.concat(" avatarEdit");
+    }
+    else{
+      localStorage.setItem('currentUser',JSON.stringify(this.currentUser)); 
+      this.updateUsersList(this.currentUser);      
       element.textContent = "EDIT PROFILE";
-
-
+      myimg.className="useravatar";
+    }
   }
-  goToFollowers()
+  chooseScreen(e)
   {
-    this.router.navigate(['followers']);
+    this.currentScreen = e ; 
   }
+  onFileChange(fileInput: any){
+    this.currentUser.profilePic = fileInput.target.files[0];
+    let reader = new FileReader();
 
+    reader.onload = (e: any) => {
+        this.currentUser.profilePic = e.target.result;
+        localStorage.setItem('currentUser',JSON.stringify(this.currentUser));              
+    }
+    reader.readAsDataURL(fileInput.target.files[0]);
+  } 
+  updateUsersList(user: User) {
+    const users: User[] = JSON.parse(localStorage.getItem('users'));
+    const index: number = users
+      .findIndex(item => item.UserID === user.UserID);
+    users[index] = user;
+    localStorage.setItem('users', JSON.stringify(users));
+  }
 }
