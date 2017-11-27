3b07792da5bd6babb0dceb3f1c366cc4f252243b
Hazem Essam <hazem.essam@gmail.com>
2017-11-27 03:19:50 +0200

    add users

---
+++ db/migrate/20171126194504_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.date :date_of_birth
+      t.string :username
+      t.string :email
+      t.timestamps
+    end
+
+    add_index :users, :username, unique: true
+  end
+end
