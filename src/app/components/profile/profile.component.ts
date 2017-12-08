a955ee9712dde9885c6c106482d717c13815aa19
unknown <khaledawaled@gmail.com>
2017-12-08 16:03:50 +0200

    followers card - profile pic upload

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -18,27 +18,35 @@ export class ProfileComponent implements OnInit {
   showWatchlistMovies: boolean;
   showRecommendedMovies: boolean;
   currentScreen:number;
+  louda:User;
+  user1:User;
   constructor(private router:Router) {
+      this.louda = new User();
+      this.user1 = new User();
       this.currentUser = JSON.parse(localStorage.getItem('currentUser'));    
-      console.log(this.currentUser); 
       this.currentUser.Email=["khaledawaled@live.com"];
       this.currentUser.Age = 21;
-      this.currentUser.bio = "";
-      //localStorage.setItem('currentUser',JSON.stringify(this.currentUser));
       this.currentScreen = 0;
       this.isEdit = false;
       this.showFollowers = false;
       this.showRatedMovies = true;
       this.showWatchlistMovies = false;
-      this.showRecommendedMovies = false;
-      this.currentUser.Friends=[]
-     // this.currentUser.Friends.push(this.currentUser);
-       localStorage.setItem('currentUser',JSON.stringify(this.currentUser));
-      console.log(this.currentUser.Friends);
-  }
-
+      this.showRecommendedMovies = false; 
+      this.louda.bio="hamada ra7 wa magash";
+      this.louda.FirstName = "louda";
+      this.louda.LastName = "hamada";
+      this.louda.profilePic = this.currentUser.profilePic;
+      this.user1.FirstName = "tftf";
+      this.user1.LastName = "2f2f";
+      this.user1.bio = "hanafy comes first";
+      this.user1.profilePic = this.currentUser.profilePic;
+      this.currentUser.Followers=[];
+      this.currentUser.Followers.push(this.louda);
+      this.currentUser.Followers.push(this.user1);
+      localStorage.setItem('currentUser',JSON.stringify(this.currentUser));
+      console.log(this.currentUser.Followers);
+      }
   ngOnInit() {
-
   }
   takeAction(element){
     this.isEdit = !this.isEdit;
@@ -54,7 +62,6 @@ export class ProfileComponent implements OnInit {
   chooseScreen(e)
   {
     this.currentScreen = e ; 
-    console.log(e)
   }
   onFileChange(fileInput: any){
     this.currentUser.profilePic = fileInput.target.files[0];
@@ -63,7 +70,6 @@ export class ProfileComponent implements OnInit {
     reader.onload = (e: any) => {
         this.currentUser.profilePic = e.target.result;
         localStorage.setItem('currentUser',JSON.stringify(this.currentUser));
-        console.log('tamam?')
         this.updateUsersList(this.currentUser);           
     }
     reader.readAsDataURL(fileInput.target.files[0]);
@@ -73,12 +79,10 @@ export class ProfileComponent implements OnInit {
     const index: number = users
       .findIndex(item => item.UserID === user.UserID);
     users[index] = user;
-    console.log('tamam')
     localStorage.setItem('users', JSON.stringify(users));
   }
 
   chooseTab(id: number){
-    console.log(id)
     if(id == 1){
       this.showRatedMovies = true;
       this.showFollowers = false;
