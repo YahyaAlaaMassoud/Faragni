b10e397d6884c47fe56c0d2a7f95a3f15511ca6c
unknown <khaledawaled@gmail.com>
2017-12-09 00:48:16 +0200

    follower and following cards , profile pic avatar

---
+++ src/app/components/profile/following/following.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { User } from '../../../models/user.model';
+
+@Component({
+  selector: 'app-following',
+  templateUrl: './following.component.html',
+  styleUrls: ['./following.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class FollowingComponent implements OnInit {
+
+  followingList:User[];
+  currentUser:User;
+  constructor() { 
+    this.currentUser=JSON.parse(localStorage.getItem("currentUser"));
+    this.followingList = this.currentUser.Following;
+    console.log(this.followingList);
+  }
+  ngOnInit() {
+  }
+
+}
+
