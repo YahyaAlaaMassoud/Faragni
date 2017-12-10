0a86cd3eeb47d29ab305df4119871c9a6b099abb
hazefully <hazem.essam@gmail.com>
2017-12-10 11:24:22 +0200

    heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+
+app.listen(process.env.PORT || 8080);
